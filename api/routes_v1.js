7e54cf50bfb8a989ab4bc287a1a35b4dd938b339
SilvanoGoossen <shmwgoos@avans.nl>
2017-06-16 14:20:05 +0200

    Routes aangepast

--- api/routes_v1.js
+++ api/routes_v1.js
@@ -55,9 +55,10 @@ routes.get('/films/:count/:start', function(req, res){
 
     res.contentType('application/json');
 
-    var query = {sql: 'SELECT * FROM film ORDER BY `film_id` ASC LIMIT ?  OFFSET ?',
+    var query = {
+        sql: 'SELECT * FROM `1033`.`film` ORDER BY `film_id` ASC LIMIT ?  OFFSET ?',
         values: [limit, offset],
-        timeout: 2000}
+        timeout: 2000};
 
     db.query(query, function(error, rows, fields) {
         if (error) {
