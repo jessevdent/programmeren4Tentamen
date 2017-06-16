7c7439ec3c6de13c6d140cadb18b1b25bce11dcf
SilvanoGoossen <shmwgoos@avans.nl>
2017-06-16 13:59:22 +0200

    Routes aangepast

--- api/routes_v1.js
+++ api/routes_v1.js
@@ -48,14 +48,14 @@ routes.get('/rentals/:userid', function(req, res){
     });
 });
 
-routes.get('/films/:count/:offset', function(req, res){
+routes.get('/films/:count/:start', function(req, res){
 
-    var count = req.params.count;
-    var offset = req.params.offset;
+    var limit = req.params.count;
+    var offset = req.params.start;
 
     res.contentType('application/json');
 
-    db.query('SELECT * FROM `1033`.`film` ORDER BY `film_id` ASC LIMIT ?  OFFSET ?', [ count, offset ], function(error, rows, fields) {
+    db.query('SELECT * FROM `1033`.`film` ORDER BY `film_id` ASC LIMIT ?  OFFSET ?', [ limit, offset ], function(error, rows, fields) {
         if (error) {
             res.status(400);
             res.json({ error: 'Error while performing Query.'});
