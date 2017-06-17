f0aa06ce5ff504b744de5987d0c43171c2dd00f2
SilvanoGoossen <shmwgoos@avans.nl>
2017-06-17 15:15:00 +0200

    Server aangepast

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.json({ type: 'application/vnd.api+json' }));
 app.use(expressJWT({ secret : config.secretkey })
 	.unless({
-		path: ['/api/v1/login', '/api/v1/register', /^\/api\/v1\/films\/.*\/.*/  , /^\/api\/v1\/films\/.*/]
+		path: ['/api/v1/login', '/api/v1/register', /^\/api\/v1\/films\/?limit=\/.*\/&offset=\/.*/  , /^\/api\/v1\/films\/.*/]
 	}));
 
 app.set('port', (process.env.PORT | config.webPort));
