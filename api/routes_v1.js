f698f5e744157af1c05c276581e1512325f1df35
Jesse van der Ent <jesse.v.d.ent@hotmail.com>
2017-06-20 01:02:05 +0200

    film endpoint aangepast

--- api/routes_v1.js
+++ api/routes_v1.js
@@ -21,7 +21,7 @@ routes.get('/films', function(req, res){
 
     res.contentType('application/json');
 
-    var query = 'SELECT * FROM `1033`.`film` ORDER BY `film`.`film_id` ASC';
+    var query = 'SELECT * FROM `1033`.`film`, `1033`.`inventory` WHERE `film`.`film_id` = `inventory`.`film_id` ORDER BY `film`.`film_id` ASC';
 
     if(limit !== undefined) {
         query += ' LIMIT ' + limit;
